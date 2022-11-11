d8104e581ae869418f89aaaa42e2b3046e5e3a34
Tom Maniglier <tom.maniglier@gmail.com>
2022-11-11 23:27:55 +0100

    first version complete

--- js/script.js
+++ js/script.js
@@ -2,9 +2,10 @@ const container = document.querySelector('.container');
 
 for (let i = 0; i < 256; i++) {
     const item = document.createElement('div');
-    container.appendChild(item);
-}
 
-function abc() {
-    item.style.backgroundColor = '#444';
+    item.addEventListener('mouseover', () => {
+        item.style.background = '#444';
+    })
+
+    container.appendChild(item);
 }
