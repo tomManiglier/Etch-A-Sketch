195d0414ac69571f8ea9aaedbf60a8a6acd78a6c
tomManiglier <tom.maniglier@gmail.com>
2022-11-11 23:11:11 +0100

    create grid

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const container = document.querySelector('.container');
+
+for (let i = 0; i < 256; i++) {
+    const item = document.createElement('div');
+    container.appendChild(item);
+}
+
+function abc() {
+    item.style.backgroundColor = '#444';
+}
